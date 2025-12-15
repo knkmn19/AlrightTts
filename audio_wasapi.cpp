f40595ce904f4d4c9d417c422c96bc4552ab1878
knkmn19 <knkmn19.code@gmail.com>
2025-12-16 08:29:17 +1300

    alrtts : audio_wasapi.cpp tu

---
+++ audio_wasapi.cpp
@@ -0,0 +1,56 @@
+/* { audio_wasapi.cpp } : audio wsasapi backend impl
+ */
+
+extern "C" {
+
+    #include "audio.h"
+
+extern "C++" {
+
+    #pragma comment(lib, "Avrt.lib")
+
+    #include <windows.h>
+    #include <mmdeviceapi.h>
+    #include <audioclient.h>
+    #include <avrt.h>
+    #include <Functiondiscoverykeys_devpkey.h>
+
+namespace {
+
+    struct ENGINE final {
+        audio_engine engine;
+    };
+
+} // {unnamed}
+
+} // extern "C++"
+
+    error audio_putdrivermeta(audio_drivermeta const** ptrdms)
+    {
+        *ptrdms = NULL;
+        return error_ok;
+    }
+
+    void audio_freedrivermeta(audio_drivermeta const** ptrdms)
+    {
+        audio_drivermeta const* dm = *ptrdms;
+        for (;;) {
+            if (dm == NULL)
+                break;
+            (void)dm;
+            dm += 1;
+        }
+    }
+
+    error audio_createenginewith(
+        audio_drivermeta const dm, audio_engine** ptra
+    )
+    {
+        (void)dm, (void)ptra;
+        return error_fail;
+    }
+
+    void audio_destroyengine(audio_engine* a)
+        { (void)a; }
+
+} // extern "C"
