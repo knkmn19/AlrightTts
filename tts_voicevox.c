a1d1c2c7a3b4905d2a8323bb0bd72324ccc78c75
knkmn19 <knkmn19.code@gmail.com>
2025-12-17 08:25:07 +1300

    alrtts : tts_voicevox.c tu

---
+++ tts_voicevox.c
@@ -0,0 +1,22 @@
+/* { tts_voicevox.c } : tts with voicevox backend
+ */
+
+#include "tts.h"
+
+#pragma comment(lib, "voicevox_core.lib")
+#pragma comment(lib, "voicevox_onnxruntime.lib")
+
+error tts_init(void)
+    { return error_ok; }
+
+void tts_destroy(void)
+    { ; }
+
+error tts_pcmfromutf8(char const* s, struct tts_pcmdesc* ptrd)
+{
+    (void)s, (void)ptrd;
+    return error_ok;
+}
+
+void tts_freepcm(struct tts_pcmdesc const d)
+    { (void)d; }
