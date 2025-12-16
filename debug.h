e41d3016c8b6209016bc56efaf653b009e9ef919
knkmn19 <knkmn19.code@gmail.com>
2025-12-17 12:08:59 +1300

    alrtts : debug.h

---
+++ debug.h
@@ -0,0 +1,17 @@
+/* { debug.h } : you guys might not know this but i have been hiding stdio
+ * includes for like 500 commits Which is why it will Finally be made visible
+ * Here
+ */
+
+#ifndef ALRTTS_DEBUG
+#define ALRTTS_DEBUG
+
+#if (_MSC_VER > 1000)
+#   pragma once
+#
+#endif /* (_MSC_VER > 1000) */
+
+#include <assert.h>
+#include <stdio.h>
+
+#endif /* !ALRTTS_DEBUG */
