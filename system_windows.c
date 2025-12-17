62bdd72c9907e2f76f26b34dc54aca33ab6332bb
knkmn19 <knkmn19.code@gmail.com>
2025-12-18 08:54:32 +1300

    alrtts : dont write to stderr in 'debug' builds  - its just easier to use ndebug but idk if this actually works

--- system_windows.c
+++ system_windows.c
@@ -4,11 +4,22 @@
 #include "system.h"
 
 #include <windows.h>
+#include "stdio.h"
 
 error console_init(void)
+#ifdef NDEBUG
+{
+    fclose(stderr);
+    (void)SetConsoleOutputCP(CP_UTF8);
+    (void)SetConsoleCP(CP_UTF8);
+
+    return error_ok;
+}
+#else
 {
     (void)SetConsoleOutputCP(CP_UTF8);
     (void)SetConsoleCP(CP_UTF8);
 
     return error_ok;
 }
+#endif
